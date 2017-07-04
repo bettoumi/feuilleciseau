a63d8fbd3f04d0f1ea68eb932d716077ec0638c4
sahbia <ouaja.sahbia@gmail.com>
2017-07-04 14:16:41 +0200

    add html/css

--- js/main.js
+++ js/main.js
@@ -1,21 +1,21 @@
-function jeux(){
-        var p1=0;
-       var p2=0;
-        var t=["feuille","ciseau","pierre"];
-       while((p1<3)&&(p2<3))
-        {
-         var choixsut=prompt('entrer votre choix');
-         var choixord=t[Math.floor(Math.random()*3)];
-         console.log("l'odinateur a choisi:  "+choixord);
-         (((choixsut==="pierre")&&(choixord==="ciseau"))|| ((choixsut==="feuille")&&(choixord==="pierre"))||((choixsut==="ciseau")&&(choixord==="feuille")))? p1+=1 :
+// function jeux(){
+//         var p1=0;
+//        var p2=0;
+//         var t=["feuille","ciseau","pierre"];
+//        while((p1<3)&&(p2<3))
+//         {
+//          var choixsut=prompt('entrer votre choix');
+//          var choixord=t[Math.floor(Math.random()*3)];
+//          console.log("l'odinateur a choisi:  "+choixord);
+//          (((choixsut==="pierre")&&(choixord==="ciseau"))|| ((choixsut==="feuille")&&(choixord==="pierre"))||((choixsut==="ciseau")&&(choixord==="feuille")))? p1+=1 :
            
-          (((choixord==="pierre")&&(choixsut==="ciseau"))|| ((choixord==="feuille")&&(choixsut==="pierre"))||((choixord==="ciseau")&&(choixsut==="feuille")))? p2+=1 : console.log("egalite");
+//           (((choixord==="pierre")&&(choixsut==="ciseau"))|| ((choixord==="feuille")&&(choixsut==="pierre"))||((choixord==="ciseau")&&(choixsut==="feuille")))? p2+=1 : console.log("egalite");
 
-        console.log("le score de l'ordinateur est: "+ p2 + " le score de sahbia est: " + p1 );
+//         console.log("le score de l'ordinateur est: "+ p2 + " le score de sahbia est: " + p1 );
 
-        }   
+//         }   
            
-        (p1>p2)? console.log("sahbia est ganiante"): console.log("l'ordinateur est ganiant");
+//         (p1>p2)? console.log("sahbia est ganiante"): console.log("l'ordinateur est ganiant");
 
-      }
-     jeux();
+//       }
+//      jeux();
