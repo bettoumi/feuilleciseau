1a0cb9a7d739e79feff626f337a13ee7729c3b8a
sahbia <ouaja.sahbia@gmail.com>
2017-07-06 16:37:15 +0200

    script

--- js/main.js
+++ js/main.js
@@ -1,25 +1,27 @@
 
      
-    // var input = document.getElementById("name");
+    //function jouer()
+    //{
      var p = document.getElementById("joueurname");
       p.innerHTML="";
-      var x = document.getElementById("name");
-     x.value = "";
+     
+     
+      var p1=0;
+      var p2=0;
 
    // input.onkeydown = function(){
    //  value = this.value;
    //  p.innerHTML = value;
    //  }
-   function saisinom() {
+     function saisinom() {
         var x = document.getElementById("name");
        x.value = x.value.toUpperCase();
        p.innerHTML = x.value;
-     }
+       }
 
 
 
-        var p1=0;
-          var p2=0;
+        
         var t=document.getElementsByClassName("choixo");
       
     
@@ -31,9 +33,10 @@
             choixut =but.id;
            
                but.classList.add("z-indexc");
+
                               
-              var choixord=t[Math.floor(Math.random()*3)]
-             // alert(choixord.alt);
+              var choixord=t[Math.floor(Math.random()*3)];
+             
            
              choixord.classList.add("z-indexO");
          
@@ -41,15 +44,17 @@
              { p1+=1 ;}
            else if(((choixord.alt==="pierre")&&(choixut==="ciseau"))|| ((choixord.alt==="feuille")&&(choixut==="pierre"))||((choixord.alt==="ciseau")&&(choixut==="feuille"))){p2+=1;} 
            document.getElementById("scorej").innerHTML=p1;
-           document.getElementById("scoreord").innerHTML=p2;
-           if(p1>=3||p2>=3)
-           {
-             alert("fin de jeux");
-           }
+           document.getElementById("scorord").innerHTML=p2;
+           
+            if(p1>=3||p2>=3)
+              if((p1==3)&&(p2=!3)) {document.getElementById("ganiant").innerHTML="WOAH "" AS GANIER";}
+              else if( (p2==3)&&(p1=!3)) {document.getElementById("ganiant").innerHTML="L'ORDINATEUR A GANIER";}
+                else {document.getElementById("ganiant").innerHTML="EGALITEE";}
+           
          }
         
-  
-           
+  //}
+      
       
       
      
