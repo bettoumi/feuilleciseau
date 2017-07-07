a45b6f5a0b31065bf2ccd8ef98409026b59c4dd7
sahbia <ouaja.sahbia@gmail.com>
2017-07-07 16:52:31 +0200

    add score function

--- js/main.js
+++ js/main.js
@@ -1,60 +1,67 @@
 
      
-    //function jouer()
-    //{
-     var p = document.getElementById("joueurname");
-      p.innerHTML="";
-     
-     
-      var p1=0;
-      var p2=0;
-
-   // input.onkeydown = function(){
-   //  value = this.value;
-   //  p.innerHTML = value;
-   //  }
-     function saisinom() {
-        var x = document.getElementById("name");
-       x.value = x.value.toUpperCase();
-       p.innerHTML = x.value;
-       }
-
-
-
-        
-        var t=document.getElementsByClassName("choixo");
-      
+    //document.getElementById("btjouer").onclick = function() {jouer()};
+    //
+   var p1=0;
+  var p2=0;
+   p=document.getElementById("joueurname");
+  
     
-        var choixut;
-        function choixClick(but)
-         {
-           
-             
-            choixut =but.id;
-           
-               but.classList.add("z-indexc");
+ function jouer()
 
-                              
-              var choixord=t[Math.floor(Math.random()*3)];
-             
-           
-             choixord.classList.add("z-indexO");
-         
-             if(((choixut==="pierre")&&(choixord.alt==="ciseau"))|| ((choixut==="feuille")&&(choixord.alt==="pierre"))||((choixut==="ciseau")&&(choixord.alt==="feuille")))
-             { p1+=1 ;}
-           else if(((choixord.alt==="pierre")&&(choixut==="ciseau"))|| ((choixord.alt==="feuille")&&(choixut==="pierre"))||((choixord.alt==="ciseau")&&(choixut==="feuille"))){p2+=1;} 
-           document.getElementById("scorej").innerHTML=p1;
-           document.getElementById("scorord").innerHTML=p2;
-           
-            if(p1>=3||p2>=3)
-              if((p1==3)&&(p2=!3)) {document.getElementById("ganiant").innerHTML="WOAH "" AS GANIER";}
-              else if( (p2==3)&&(p1=!3)) {document.getElementById("ganiant").innerHTML="L'ORDINATEUR A GANIER";}
-                else {document.getElementById("ganiant").innerHTML="EGALITEE";}
-           
-         }
+{
+     p1=0;
+     p2=0;
+    
+   document.getElementById("name").value="";
+    document.getElementById("scorej").innerHTML=0;
+  document.getElementById("scorord").innerHTML=0;
+
+}  
+function saisinom() {
+        var x = document.getElementById("name");
+        x.value = x.value.toUpperCase();
+        p.innerHTML =x.value;
+       }
+function choixClick(but) {
+      
         
-  //}
-      
-      
-      
+  saisinom();
      
+     var u=but.id;
+
+     
+        var t=document.getElementsByClassName("choixo");
+        var o=t[Math.floor(Math.random()*3)];
+  
+
+       if(((u==="pierre")&&(o.alt==="ciseau"))||
+        ((u==="feuille")&&(o.alt==="pierre"))||
+        ((u==="ciseau")&&(o.alt==="feuille")))
+             { p1+=1 ;}
+           else if(((o.alt==="pierre")&&(u==="ciseau"))||
+            ((o.alt==="feuille")&&(u==="pierre"))||
+            ((o.alt==="ciseau")&&(u==="feuille"))){p2+=1;} 
+           document.getElementById("scorej").innerHTML=p1;
+          document.getElementById("scorord").innerHTML=p2;
+    
+           
+          
+
+         if(p1>=3||p2>=3){
+              if((p1==3)&&(p2<33)) 
+                {document.getElementById("ganiant").innerHTML="WOAH "+ p.innerHTML +" A GANIER";
+                    jouer() ;  }
+              else if( (p2==3)&&(p1<3))
+               {document.getElementById("ganiant").innerHTML="L'ORDINATEUR A GANIER";
+
+                 jouer();}
+                else if((p1==3)&&(p2==3)){document.getElementById("ganiant").innerHTML="EGALITEE";
+               jouer();
+
+               }}
+
+
+     }
+
+    
